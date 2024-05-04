dd35ba366e0f7dfbe85a15ff551b2e7f324ba68e
bechirMakhlouf <bechirM@proton.me>
2024-05-04 22:25:21 +0100

    fix roles transform string into array

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,35 +9,33 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
-      ->add('roles', ChoiceType::class, array(
-        'ROLE_ADMIN' => array(
-          'Yes' => 'ROLE_ADMIN'
-        ),
-        'ROLE_USER' => array(
-          'Yes' => 'ROLE_STUDENT'
-        )
-      ))
+            ->add('roles', ChoiceType::class, array(
+              'choices' => array(
+                  'user' => 'ROLE_USER',
+                  'admin' => 'ROLE_ADMIN'
+              ),
+              'label' => 'Role :'
+              ))
             ->add('password')
         ;
 
-        // $builder->get('roles')
-        //     ->addModelTransformer(new CallbackTransformer(
-        //         function ($rolesArray) {
-        //             // transform the array to a string
-        //             return count($rolesArray) ? $rolesArray[0] : null;
-        //         },
-        //         function ($rolesString) {
-        //             // transform the string back to an array
-        //             return [$rolesString];
-        //         }
-        //     ));
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray) ? $rolesArray[0] : null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
 
     }
     public function configureOptions(OptionsResolver $resolver): void
