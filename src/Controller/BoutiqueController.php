852a11ffb2eb36873beb715b90fff9e46ed91263
bechirMakhlouf <bechirM@proton.me>
2024-05-08 19:00:35 +0100

    add pagination

--- src/Controller/BoutiqueController.php
+++ src/Controller/BoutiqueController.php
@@ -2,22 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Livres;
 use App\Repository\LivresRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BoutiqueController extends AbstractController
 {
-    #[Route('/boutique', name: 'app_boutique')]
-    public function index(LivresRepository $livresRepository): Response
+    // #[Route('/boutique', name: 'app_boutique')]
+    // public function index(LivresRepository $livresRepository): Response
+    // {
+    //     // return $this->render('livres/index.html.twig', [
+    //     //     'livres' => $livresRepository->findAll(),
+    //     // ]);
+    //     return $this->render('boutique/index.html.twig', [
+    //         'controller_name' => 'BoutiqueController',
+    //         'livres' => $livresRepository->findAll(),
+    //     ]);
+    // }
+
+    #[Route('/boutique/{page<\d+>?1}', name: 'app_boutique')]
+    public function index(LivresRepository $livresRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        // return $this->render('livres/index.html.twig', [
-        //     'livres' => $livresRepository->findAll(),
-        // ]);
-        return $this->render('boutique/index.html.twig', [
-            'controller_name' => 'BoutiqueController',
-            'livres' => $livresRepository->findAll(),
+        $query = $livresRepository->createQueryBuilder('l')->getQuery();
+        $pagination = $paginator->paginate(
+            $query, 
+            $request->attributes->get('page', 1), 
+            10 
+        );
+
+        return $this->render('boutique/index2.html.twig', [
+            'pagination' => $pagination,
         ]);
     }
+
+    #[Route('/boutique/livre/{id}', name: 'app_boutique_livre', methods: ['GET'])]
+    public function show(Livres $livre): Response
+    {
+        return $this->render('boutique/livre.html.twig', [
+            'livre' => $livre,
+        ]);
+    }
+
 }
